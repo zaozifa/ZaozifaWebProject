89ec62fb4458dbc98b8e4877b9aafc7574d9d273
zhangxinwei <597661233@qq.com>
2020-08-09 13:18:45 +0800

    update

--- src/main/java/ZI.java
+++ src/main/java/ZI.java
@@ -66,29 +66,38 @@ public class ZI {
                     printWriter.write("<html lang=\"zh\">\n");
                     printWriter.write("<head>\n");
                     printWriter.write("    <meta charset=\"UTF-8\">\n");
-                    printWriter.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no\" />\n");
+                    printWriter.write("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
                     printWriter.write("    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n");
-                    printWriter.write("    <link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\" />\n");
+                    printWriter.write("    <meta name=\"keywords\" content=\"汉字，说文解字，甲骨文\">\n");
+                    printWriter.write("    <meta name=\"description\" content=\"造字法，汉字，简体字，甲骨文，金文，异体字，音韵方言，部首笔画，康熙字典，说文解字，字源字形\" />\n");
+                    printWriter.write("    <link rel=\"shortcut icon\" href=\"/favicon.ico\" type=\"image/x-icon\" />\n");
+                    printWriter.write(" <script src=\"zaozifa.js\"></script>\n");
+                    printWriter.write("");
                     printWriter.write("<title>");
                     printWriter.write(zi);
                     printWriter.write(" - 造字法</title>\n");
-                    printWriter.write("        <link rel=\"stylesheet\" href=\"../../zaozifa.css\">\n");
-                    printWriter.write("    <script src=\"zaozifa.js\"></script>\n");
+                    printWriter.write("    <link  type=\"text/css\" rel=\"stylesheet\" href=\"/zaozifa.css\"/>\n");
+                    printWriter.write("    <script src=\"/zaozifa.js\"></script>\n");
                     printWriter.write("</head>\n");
                     printWriter.write("<body>\n");
                     printWriter.write("<div id=\"wrap\">\n");
                     printWriter.write("    <div id=\"header\" class=\"\">\n");
-                    printWriter.write("        <img alt=\"zaozifa logo\" src=\"logo.png\" width=\"64px\" height=\"64px\">\n");
+                    printWriter.write("        <img alt=\"zaozifa logo\" src=\"/logo.png\" width=\"64px\" height=\"64px\">\n");
                     printWriter.write("        <h2><a href=\"/\">造字法</a></h2>\n");
                     printWriter.write("        <p id=\"subtitle\" class=\"meta\"><strong>造字原理基于说文解字、甲骨文、金文、竹简汉字</strong></p>\n");
-                    printWriter.write("        <p id=\"nav\">");
-                    printWriter.write("            <a href=\"#\" class=\"logo\">首页</a>\n");
-                    printWriter.write("            <a href=\"#\" class=\"button\">汉字按部索引</a>\n");
-                    printWriter.write("            <a href=\"#\" class=\"button\">相关古书</a>\n");
+                    printWriter.write("        <p id=\"nav\">\n");
+                    printWriter.write("            <a href=\"/\">首页</a>\n");
+                    printWriter.write("            <span>·</span>\n");
+                    printWriter.write("            <a href=\"/ABCD.html\">始于象形</a>\n");
+                    printWriter.write("            <span>·</span>\n");
+                    printWriter.write("            <a href=\"/lastupdate.html\">更新</a>\n");
+                    printWriter.write("            <span>·</span>\n");
+                    printWriter.write("            <a href=\"/readme.html\">注意</a>\n");
                     printWriter.write("        </p>\n");
-                    printWriter.write("        <div>\n");
-                    printWriter.write("            <input type=\"search\" name=\"search-bar\" id=\"search-bar\" placeholder=\"Search Homebrew\"/>\n");
-                    printWriter.write("            <button>submit</button>\n");
+                    printWriter.write("        <div id=\"searchbox\">\n");
+                    printWriter.write("            <input type=\"text\" id=\"search\"/>\n");
+                    printWriter.write("            <button>Baidu</button>\n");
+                    printWriter.write("            <button>Google</button>\n");
                     printWriter.write("        </div>\n");
                     printWriter.write("    </div>\n");
                     printWriter.write("    <div id=\"content\">\n");
@@ -96,31 +105,31 @@ public class ZI {
                     printWriter.write(zi);
                     printWriter.write("</h1>\n");
                     printWriter.write("        <p>\n");
-                    printWriter.write("            <img src=\"ziimg/whan.png\"/>\n");
+                    printWriter.write("            <img src=\"/ziimg/whan.png\"/>\n");
                     printWriter.write("        </p>\n");
-                    printWriter.write("        <p class=\"green\">发声：");
+                    printWriter.write("        <p class=\"green\"><strong class=\"red\">发声：");
                     printWriter.write(yin==null?"":yin);
                     printWriter.write("</p>\n");
-                    printWriter.write("        <p class=\"green\">部件：");
+                    printWriter.write("        <p class=\"green\"><strong class=\"red\">部件：");
                     printWriter.write(jian);
                     printWriter.write("</p>\n");
-                    printWriter.write("        <p><strong class=\"red\">造字法");
+                    printWriter.write("        <p><strong class=\"red\">【造字法】");
                     printWriter.write(zi);
                     printWriter.write("：</strong>");
                     printWriter.write(comments);
                     printWriter.write("        </p>\n");
-                    printWriter.write("        <p><strong class=\"meta\">说文解字");
+                    printWriter.write("        <p><strong class=\"meta\">【说文解字】");
                     printWriter.write(zi);
                     printWriter.write("：</strong>");
                     printWriter.write(js);
-                    printWriter.write("        <div id=\"kxzd\"><strong class=\"meta\">康熙字典");
+                    printWriter.write("        <div id=\"kxzd\"><strong class=\"meta\">【康熙字典】");
                     printWriter.write(zi);
                     printWriter.write("：</strong><br>\n");
                     printWriter.write("            <p>\n");
                     printWriter.write(kx == null ?"":kx);
                     printWriter.write("            </p>\n");
                     printWriter.write("        </div>\n");
-                    printWriter.write("        <div id=\"gswy\"><strong class=\"meta\">古书物语");
+                    printWriter.write("        <div id=\"gswy\"><strong class=\"meta\">【古书物语】");
                     printWriter.write(zi);
                     printWriter.write("：</strong><br>\n");
                     printWriter.write("            <p>\n");
@@ -129,40 +138,21 @@ public class ZI {
                     printWriter.write("        </div>\n");
                     printWriter.write("    </div>\n");
                     printWriter.write("    <div style=\"padding: 2em\">\n");
-                    printWriter.write("        <a href=\"dasang.html\">赏</a>\n");
+                    printWriter.write("        <a href=\"/dasang.html\">赏</a>\n");
                     printWriter.write("    </div>\n");
                     printWriter.write("    <div id=\"footer\">\n");
                     printWriter.write("        <p>\n");
-                    printWriter.write("            <a>历史版本</a>\n");
-                    printWriter.write("            <a href=\"about.html\">关于本站</a>\n");
-                    printWriter.write("<select>\n" +
-                            "                <option value=\"\">字体</option>\n" +
-                            "                <option value=\"\">宋体</option>\n" +
-                            "                <option value=\"\">微软雅黑</option>\n" +
-                            "                <option value=\"\">黑体</option>\n" +
-                            "                <option value=\"\">隶体</option>\n" +
-                            "                <option value=\"\">隶体</option>\n" +
-                            "            </select>\n" +
-                            "            <select>\n" +
-                            "                <option value=\"\">字大小</option>\n" +
-                            "                <option value=\"\">14px</option>\n" +
-                            "                <option value=\"\">16px</option>\n" +
-                            "                <option value=\"\">18px</option>\n" +
-                            "                <option value=\"\">20px</option>\n" +
-                            "                <option value=\"\">22px</option>\n" +
-                            "                <option value=\"\">24px</option>\n" +
-                            "                <option value=\"\">26px</option>\n" +
-                            "                <option value=\"\">28px</option>\n" +
-                            "                <option value=\"\">30px</option>\n" +
-                            "            </select>\n" +
-                            "        </p>\n" +
-                            "    </div>\n" +
-                            "</div>\n" +
-                            "<script>\n" +
-                            "    loadSuccessThen();\n" +
-                            "</script>\n" +
-                            "</body>\n" +
-                            "</html>");
+                    printWriter.write("            <a href=\"https://github.com/zaozifa/ZaozifaWebProject\">历史版本</a>\n");
+                    printWriter.write("            <span>·</span>\n");
+                    printWriter.write("            <a href=\"/about.html\">关于本站</a>\n");
+                    printWriter.write("        </p>\n");
+                    printWriter.write("    </div>\n");
+                    printWriter.write("</div>\n");
+                    printWriter.write("<script>\n");
+                    printWriter.write("    loadSuccessThen();\n");
+                    printWriter.write("</script>\n");
+                    printWriter.write("</body>\n");
+                    printWriter.write("</html>");
                     printWriter.flush();
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
---
+++ src/main/java/util/MyPDFUtil.java
@@ -0,0 +1,60 @@
+package util;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.PdfCopy;
+import com.lowagie.text.pdf.PdfImportedPage;
+import com.lowagie.text.pdf.PdfReader;
+
+/**
+ * 截取pdf 的第from页至第end页，组成一个新的文件名
+ */
+public class MyPDFUtil {
+
+    public static void main(String[] args) {
+        partitionPdfFile("C:\\Users\\zhangxinwei\\IdeaProjects\\ZaozifaWebProject\\webPage\\pdf.pdf", "Chapter04.pdf", 31, 32);
+    }
+
+    /**
+     * 截取pdfFile的第from页至第end页，组成一个新的文件名
+     *
+     * @param pdfFile
+     * @param from
+     * @param end
+     */
+    public static void partitionPdfFile(String pdfFile,
+                                        String newFile, int from, int end) {
+        Document document = null;
+        PdfCopy copy = null;
+        try {
+            PdfReader reader = new PdfReader(pdfFile);
+            int n = reader.getNumberOfPages();
+            if (end == 0) {
+                end = n;
+            }
+            ArrayList<String> savepaths = new ArrayList<String>();
+            String staticpath = pdfFile.substring(0, pdfFile.lastIndexOf("\\") + 1);
+            String savepath = staticpath + newFile;
+            savepaths.add(savepath);
+            document = new Document(reader.getPageSize(1));
+            copy = new PdfCopy(document, new FileOutputStream(savepaths.get(0)));
+            document.open();
+            for (int j = from; j <= end; j++) {
+                document.newPage();
+                PdfImportedPage page = copy.getImportedPage(reader, j);
+                copy.addPage(page);
+            }
+            document.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+    }
+
+}  
