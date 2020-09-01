00ba45a4330d840bef8d5cccf20c04a621863c50
zhangxinwei <597661233@qq.com>
2020-09-01 20:25:44 +0800

    语法句式 & 字

--- webPage/zaozifa.js
+++ webPage/zaozifa.js
@@ -1,3 +1,36 @@
 function loadSuccessThen() {
-    
+    var url = window.location.href;
+    url = decodeURI(url);
+    var pIdx = url.indexOf("hanyuyufa");
+    if (pIdx > 0) {
+        var lastIdx = url.indexOf(".html");
+        var k = url.substring(pIdx + 10, lastIdx);
+        var content = document.getElementById("content");
+        hLight(content.getElementsByTagName("p")[0], k);
+    }else {
+        var gswy = document.getElementById("gswy");
+        if (gswy) {
+            var h1 = document.getElementsByTagName("h1")[0];
+            var zis = h1.innerHTML.split('');
+            hLightKs(gswy.getElementsByTagName("p")[0], zis);
+        }
+    }
+}
+
+function hLight(content, k) {
+    var contents = content.innerHTML;
+    var values = contents.split(k);
+    content.innerHTML = values.join('<span style="background:rgb(208, 102, 81);">' + k + '</span>');
+}
+
+function hLightKs(content, ks) {
+    var contents = content.innerHTML;
+    for (var k in ks) {
+        var values = contents.split(ks[k]);
+        if (values.length > 1) {
+            content.innerHTML = values.join('<span style="background:rgb(208, 102, 81);">' + ks[k] + '</span>');
+            break;
+        }
+    }
+
 }
